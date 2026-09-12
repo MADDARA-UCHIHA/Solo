847e59608383c30fe94fac212fb5ba52ab3ca1a3
MADDARA-UCHIHA <ghero8695@gmail.com>
2026-09-12 15:24:33 +0300

    Fix encoding and lock files to LF line endings

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    kotlin("kapt") version "2.0.0" apply false
+    // kapt o'rniga KSP ni qo'shamiz (versiyasi Kotlin 2.0.0 ga mos bo'lishi kerak):
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
